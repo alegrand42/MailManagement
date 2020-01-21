7327891c2341594c13e2827295b0533a4f11d4f5
alegrand42 <alegrand@student.42.fr>
2020-01-21 16:31:30 +0100

    wip

--- app/services/mails.rb
+++ app/services/mails.rb
@@ -2,20 +2,20 @@ class Mails
   MAIL_ATTR = %w{subject to from template_id}.freeze
   VARIABLES = %w{court_date civility denomination}.freeze
 
-  def self.gather_data params
+  def self.build params
     mail = Mail.find_or_create_by(fetch_attributes(params))
     return unless mail
-    #Variable.find_or_create_by fetch_variables(params, mail.id)
+    Variable.find_or_create_by(fetch_variables(params, mail.id))
     # send mail and update deliver_at
     mail.id
   end
 
-  def self.get_mail_data(params)
-    Mail.find_by(id: params['id'])
+  def self.fetch_show(params)
+    display_single(params['id'])
   end
 
-  def self.get_all_mails_data
-    Mail.includes(:variables).to_a
+  def self.gather_all
+    Mail.includes(:variable).to_a
   end
 
   private
@@ -33,7 +33,53 @@ class Mails
       VARIABLES.each do |attr|
         hash[attr] = params[attr].strip if params[attr]
       end
-      hash['mail'] = "#{mail_id}"
+      hash['mail_id'] = "#{mail_id}"
     end
+  end
+
+  def self.display_single mail_id
+    mail = Mail.find_by(id: mail_id)
+    return unless mail
+    variables = gather_variables(mail.id)
+
+    {}.tap do |hash|
+      MAIL_ATTR.each do |attr|
+        hash[attr] = mail[attr] if attr != 'template_id'
+      end
+      hash['code'] = fetch_code(mail.template_id, variables)
+      hash['variables'] = variables
+    end
+  end
+
+  def self.fetch_code(template_id, variables)
+    template = Template.find_by(id: template_id)
+    return unless template
+    return template.mail unless variables
+
+    replace = all_replace(variables) 
+    replace.each do |rep|
+      template.mail.gsub!(rep[0], rep[1]) unless rep[1].nil?
+    end
+    template.mail
+  end
+
+  def self.gather_variables(mail_id)
+    #model
+    variables = Variable.find_by(mail_id: mail_id)
+    return unless variables
+
+    {
+      court_date: variables[:court_date],
+      civility: variables[:civility],
+      denomination: variables[:denomination]
+    }
+  end
+
+  def self.all_replace(variables)
+    [
+      ['<court_date>', variables[:court_date]],
+      ['<civility>', variables[:civility]],
+      ['<denomination>', variables[:denomination]]
+    ]
   end
 end
